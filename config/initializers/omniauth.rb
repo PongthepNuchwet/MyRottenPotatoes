9d0a12702d14032f6a2cfb17196e887a16da8107
PongthepNuchwet <jt061220@gmail.com>
2022-02-07 09:33:34 +0700

    facebook api config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     # provider :developer unless Rails.env.production?
     provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 
   end
 OmniAuth.config.on_failure = Proc.new { |env|
