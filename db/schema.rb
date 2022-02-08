feab91bb8ddc281b107f3e22bbc80490c450f9f2
PongthepNuchwet <jt061220@gmail.com>
2022-02-08 21:22:32 +0700

    Modified database username After ps successed

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_01_034820) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "moviegoers", force: :cascade do |t|
     t.string "name"
     t.string "provider"
@@ -32,8 +35,8 @@ ActiveRecord::Schema.define(version: 2022_02_01_034820) do
   create_table "reviews", force: :cascade do |t|
     t.integer "potatoes"
     t.text "comments"
-    t.integer "moviegoer_id", null: false
-    t.integer "movie_id", null: false
+    t.bigint "moviegoer_id", null: false
+    t.bigint "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
